e8dbc5502b71c433702874c807a598597fb098c9
Alex Dorrington <ajdorrington18@gmail.com>
2024-01-30 21:19:00 +0000

    day9

--- 2023/day9/approach.js
+++ 2023/day9/approach.js
@@ -11,20 +11,13 @@ const rows = fs.readFileSync('input.txt', 'utf8').split('\n').reduce((acc, c) =>
 }, [])
 
 
+
 /**
  * Question 1
  */
 const sequences = rows
 
-const checkDepth = (sequence) => {
-    return sequence.reduce((acc, c, i, arr) => {
-        if (i == 0) {
-            return acc;
-        } 
-        acc.push(arr[i] - arr[i - 1])
-        return acc;
-    }, [])
-}
+const checkDepth = (sequence) => sequence.reduce((acc, c, i, arr) => i > 0 ? (acc.push(c - arr[i - 1]), acc) : acc, [])
 
 let total = 0;
 
@@ -46,6 +39,30 @@ for (const sequence of sequences) {
 console.log(total)
 
 
+
 /**
  * Question 2
  */
+const sequences2 = rows;
+
+const checkDepth2 = (sequence) => sequence.reduce((acc, c, i, arr) => i > 0 ? (acc.push(c - arr[i - 1]), acc) : acc, [])
+
+let total2 = 0;
+
+for (const sequence of sequences2) {
+    let isAllZero = false;
+    let depth = [sequence];
+    while(!isAllZero) {
+        const diffs = checkDepth2(depth[depth.length - 1]);
+        isAllZero = diffs.every((x) => !x);
+        depth.push(diffs);
+    }
+
+    let newFirst = 0;
+    for (let i = depth.length - 1; i > 0; i--) { 
+        newFirst = depth[i - 1][0] - newFirst
+    }
+    total2 += newFirst;
+}
+
+console.log(total2)
